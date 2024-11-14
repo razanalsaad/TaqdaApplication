ef11d3f4f3ca78320c2525181e0001130aa50abd
razanalsaad <razan_alsaad@hotmail.com>
2024-11-14 21:03:30 +0300

    Initial Commit

---
+++ TaqdaApplication/TaqdaApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaqdaApplicationApp.swift
+//  TaqdaApplication
+//
+//  Created by razan on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaqdaApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaqdaApplicationTests/TaqdaApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaqdaApplicationTests.swift
+//  TaqdaApplicationTests
+//
+//  Created by razan on 14/11/2024.
+//
+
+import Testing
+@testable import TaqdaApplication
+
+struct TaqdaApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
